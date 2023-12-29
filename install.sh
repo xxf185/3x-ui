8d460cf62cdbd0ab37d249ec8be2dbfdcc9fca90
xxf185 <109097973+xxf185@users.noreply.github.com>
2023-12-29 08:53:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,28 +21,28 @@ else
     echo "未检测到系统版本，请联系脚本作者！" >&2
     exit 1
 fi
-echo "The OS release is: $release"
+echo "操作系统版本是: $release"
 
 arch3xui() {
     case "$(uname -m)" in
     x86_64 | x64 | amd64) echo 'amd64' ;;
     armv8* | armv8 | arm64 | aarch64) echo 'arm64' ;;
     armv7* | armv7 | arm | arm32 ) echo 'arm' ;;
-    *) echo -e "${green}Unsupported CPU architecture! ${plain}" && rm -f install.sh && exit 1 ;;
+    *) echo -e "${green}不支持的CPU架构！ ${plain}" && rm -f install.sh && exit 1 ;;
     esac
 }
-echo "arch: $(arch3xui)"
+echo "架构: $(arch3xui)"
 
 os_version=""
 os_version=$(grep -i version_id /etc/os-release | cut -d \" -f2 | cut -d . -f1)
 
 if [[ "${release}" == "centos" ]]; then
     if [[ ${os_version} -lt 8 ]]; then
-        echo -e "${red} Please use CentOS 8 or higher ${plain}\n" && exit 1
+        echo -e "${red} 请使用CentOS 8或更高版本 ${plain}\n" && exit 1
     fi
 elif [[ "${release}" == "ubuntu" ]]; then
     if [[ ${os_version} -lt 20 ]]; then
-        echo -e "${red}please use Ubuntu 20 or higher version!${plain}\n" && exit 1
+        echo -e "${red}请使用Ubuntu 20或更高版本!${plain}\n" && exit 1
     fi
 
 elif [[ "${release}" == "fedora" ]]; then
@@ -52,12 +52,12 @@ elif [[ "${release}" == "fedora" ]]; then
 
 elif [[ "${release}" == "debian" ]]; then
     if [[ ${os_version} -lt 10 ]]; then
-        echo -e "${red} Please use Debian 10 or higher ${plain}\n" && exit 1
+        echo -e "${red} 请使用 Debian 10 或更高版本 ${plain}\n" && exit 1
     fi
 
 elif [[ "${release}" == "almalinux" ]]; then
     if [[ ${os_version} -lt 9 ]]; then
-        echo -e "${red} Please use AlmaLinux 9 or higher ${plain}\n" && exit 1
+        echo -e "${red} 请使用AlmaLinux 9或更高版本${plain}\n" && exit 1
     fi
 elif [[ "${release}" == "arch" ]]; then
     echo "Your OS is ArchLinux"
@@ -67,7 +67,7 @@ elif [[ "${release}" == "armbian" ]]; then
     echo "Your OS is Armbian"
 
 else
-    echo -e "${red}Failed to check the OS version, please contact the author!${plain}" && exit 1
+    echo -e "${red}无法检查操作系统版本，请联系作者！${plain}" && exit 1
 fi
 
 install_base() {
@@ -126,22 +126,22 @@ install_x-ui() {
     if [ $# == 0 ]; then
         last_version=$(curl -Ls "https://api.github.com/repos/xxf185/3x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
         if [[ ! -n "$last_version" ]]; then
-            echo -e "${red}Failed to fetch x-ui version, it maybe due to Github API restrictions, please try it later${plain}"
+            echo -e "${red}获取x-ui版本失败，可能是Github API限制，请稍后再试${plain}"
             exit 1
         fi
-        echo -e "Got x-ui latest version: ${last_version}, beginning the installation..."
+        echo -e "获取 x-ui 最新版本：${last_version}，开始安装..."
         wget -N --no-check-certificate -O /usr/local/x-ui-linux-$(arch3xui).tar.gz https://github.com/xxf185/3x-ui/releases/download/${last_version}/x-ui-linux-$(arch3xui).tar.gz
         if [[ $? -ne 0 ]]; then
-            echo -e "${red}Downloading x-ui failed, please be sure that your server can access Github ${plain}"
+            echo -e "${red}下载x-ui失败，请确保您的服务器可以访问Github ${plain}"
             exit 1
         fi
     else
         last_version=$1
         url="https://github.com/xxf185/3x-ui/releases/download/${last_version}/x-ui-linux-$(arch3xui).tar.gz"
-        echo -e "Begining to install x-ui $1"
+        echo -e "开始安装x-ui $1"
         wget -N --no-check-certificate -O /usr/local/x-ui-linux-$(arch3xui).tar.gz ${url}
         if [[ $? -ne 0 ]]; then
-            echo -e "${red}Download x-ui $1 failed,please check the version exists ${plain}"
+            echo -e "${red}下载x-ui $1失败，请检查版本是否存在${plain}"
             exit 1
         fi
     fi
@@ -160,11 +160,11 @@ install_x-ui() {
     chmod +x /usr/local/x-ui/x-ui.sh
     chmod +x /usr/bin/x-ui
     config_after_install
-    #echo -e "If it is a new installation, the default web port is ${green}2053${plain}, The username and password are ${green}admin${plain} by default"
-    #echo -e "Please make sure that this port is not occupied by other procedures,${yellow} And make sure that port 2053 has been released${plain}"
-    #    echo -e "If you want to modify the 2053 to other ports and enter the x-ui command to modify it, you must also ensure that the port you modify is also released"
+    #echo -e "如果是新安装，默认Web端口为${green}2053${plain}，用户名和密码默认为${green}admin${plain}"
+    #echo -e "请确保该端口没有被其他程序占用，${yellow} 并确保2053端口已被释放${plain}"
+    #    echo -e "如果你想修改2053为其他端口，输入x-ui命令修改，还必须保证你修改的端口也被释放"
     #echo -e ""
-    #echo -e "If it is updated panel, access the panel in your previous way"
+    #echo -e "如果是更新面板，请按照之前的方式访问面板"
     #echo -e ""
     systemctl daemon-reload
     systemctl enable x-ui
@@ -181,7 +181,7 @@ install_x-ui() {
     echo -e "x-ui enable       - 设置 x-ui 开机自启"
     echo -e "x-ui disable      - 取消 x-ui 开机自启"
     echo -e "x-ui log          - 查看 x-ui 日志"
-    echo -e "x-ui banlog       - Check Fail2ban ban logs"
+    echo -e "x-ui banlog       - 查看Fail2ban禁止日志"
     echo -e "x-ui update       - 更新 x-ui 面板"
     echo -e "x-ui install      - 安装 x-ui 面板"
     echo -e "x-ui uninstall    - 卸载 x-ui 面板"
